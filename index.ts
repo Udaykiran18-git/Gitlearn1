1449b1730ad19966e0db83f10410e9e0bec7789b
uday kiran <mudayk@infinite.com>
2026-07-27 22:02:03 +0530

    file is added

---
+++ index.ts
@@ -0,0 +1,23 @@
+function add(a:number,b:number):number
+{
+        return a+b
+}
+
+const person:{
+    name:string
+    age:number
+    } = {name:"uday",age:25}
+
+
+    class student{
+         name1:string
+        constructor(name1:string)
+        {
+                this.name1=name1
+        }
+    }
+
+    const st1=new student("uday")
+
+    console.log((st1.name1));
+
